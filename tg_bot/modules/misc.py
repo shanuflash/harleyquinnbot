66d7d8bf0fdb56d2df7fc4a578694d6d310eb26d
Shanu Flash <shanuflash@gmail.com>
2018-06-19 19:56:35 +0530

    Add getlink slist and snipe ;-;

--- tg_bot/modules/misc.py
+++ tg_bot/modules/misc.py
@@ -329,6 +329,50 @@ def ping(bot: Bot, update: Update):
     update.effective_message.reply_text(" Ping speed was: {}ms".format(ping_time))
 
 
+@run_async
+def snipe(bot: Bot, update: Update, args: List[str]):
+    try:
+        chat_id = str(args[0])
+        del args[0]
+    except TypeError as excp:
+        update.effective_message.reply_text("Please give me a chat to echo to!")
+    to_send = " ".join(args)
+    if len(to_send) >= 2:
+        try:
+            bot.sendMessage(int(chat_id), str(to_send))
+        except TelegramError:
+            LOGGER.warning("Couldn't send to group %s", str(chat_id))
+            update.effective_message.reply_text("Couldn't send the message. Perhaps I'm not part of that group?")
+
+
+@run_async
+@bot_admin
+def getlink(bot: Bot, update: Update, args: List[int]):
+    if args:
+        chat_id = int(args[0])
+    else:
+        update.effective_message.reply_text("You don't seem to be referring to a chat")
+    chat = bot.getChat(chat_id)
+    bot_member = chat.get_member(bot.id)
+    if bot_member.can_invite_users:
+        invitelink = bot.exportChatInviteLink(chat_id)
+        update.effective_message.reply_text(invitelink)
+    else:
+        update.effective_message.reply_text("I don't have access to the invite link!")
+
+@run_async
+def sudolist(bot: Bot, update: Update):
+    text = "My sudo users are *{}*:"
+    for user_id in SUDO_USERS:
+        user = bot.get_chat(user_id)
+        name = "[{}](tg://user?id={})".format(user.first_name + (user.last_name or ""), user.id)
+        if user.username:
+            name = escape_markdown("@" + user.username)
+        text += "\n - {}".format(name)
+
+    update.effective_message.reply_text(text, parse_mode=ParseMode.MARKDOWN)
+
+
 MARKDOWN_HELP = """
 Markdown is a very powerful formatting tool supported by telegram. {} has some enhancements, to make sure that \
 saved messages are correctly parsed, and to allow you to create buttons.
@@ -392,7 +436,10 @@ INFO_HANDLER = DisableAbleCommandHandler("info", info, pass_args=True)
 
 PING_HANDLER = DisableAbleCommandHandler("ping", ping)
 ECHO_HANDLER = CommandHandler("echo", echo, filters=Filters.user(OWNER_ID))
+SNIPE_HANDLER = CommandHandler("snipe", snipe, pass_args=True, filters=CustomFilters.sudo_filter)
 MD_HELP_HANDLER = CommandHandler("markdownhelp", markdown_help, filters=Filters.private)
+GETLINK_HANDLER = CommandHandler("getlink", getlink, pass_args=True, filters=Filters.user(OWNER_ID))
+SLIST_HANDLER = CommandHandler("sudolist", sudolist, filters=Filters.user(OWNER_ID))
 
 STATS_HANDLER = CommandHandler("stats", stats, filters=CustomFilters.sudo_filter)
 
@@ -406,3 +453,6 @@ dispatcher.add_handler(INFO_HANDLER)
 dispatcher.add_handler(ECHO_HANDLER)
 dispatcher.add_handler(MD_HELP_HANDLER)
 dispatcher.add_handler(STATS_HANDLER)
+dispatcher.add_handler(GETLINK_HANDLER)
+dispatcher.add_handler(SLIST_HANDLER)
+dispatcher.add_handler(SNIPE_HANDLER)
