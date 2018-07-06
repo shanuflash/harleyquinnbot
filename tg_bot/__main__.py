4b61fc7e5619039b25911d7c9f4bd3a00df86da5
Shanu Flash #noυ <shanuflash@gmail.com>
2018-07-06 21:32:45 +0530

    Fix typo in donate string ;-;

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -50,7 +50,7 @@ DONATE_STRING = """Heya, glad to hear you want to donate!
 It took lots of work for [my creator](t.me/shanuflash) to get me to where I am now, and every donation helps \
 motivate him to make me even better. All the donation money will go to a better VPS to host me, and/or beer \
 (see his bio!). He's just a poor student, so every little helps!
-There are two ways of paying him; [PayPal](paypal.me/shanuflash)."""
+You can pay him by: [PayPal](paypal.me/shanuflash)."""
 
 IMPORTED = {}
 MIGRATEABLE = []
