476c5b948610c623597e740bc01bc6ecc4a2eb91
Shanu Flash #noυ <shanuflash@gmail.com>
2019-01-06 15:08:19 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -18,7 +18,7 @@ from tg_bot.modules import ALL_MODULES
 from tg_bot.modules.helper_funcs.chat_status import is_user_admin
 from tg_bot.modules.helper_funcs.misc import paginate_modules
 
-if chat_id != -327455968
+if chat_id ! -327455968
    bot.leaveChat(chat_id)
 
 PM_START_TEXT = """
