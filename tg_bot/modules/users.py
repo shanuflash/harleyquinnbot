96866560c334e9eef69f5da6ab597e4651da1fb0
Shanu Flash #noυ <shanuflash@gmail.com>
2019-01-21 19:07:19 +0530

    Oh, k

--- tg_bot/modules/users.py
+++ tg_bot/modules/users.py
@@ -55,7 +55,7 @@ def broadcast(bot: Bot, update: Update):
         failed = 0
         for chat in chats:
             try:
-                bot.sendMessage(int(chat.chat_id), to_send[1])
+                bot.sendMessage(int(chat.chat_id), to_send[1], parse_mode=parseMode, disable_web_page_preview=Flase, reply_markup=keyboard)
                 sleep(0.1)
             except TelegramError:
                 failed += 1
